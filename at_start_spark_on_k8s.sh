375cec2baea3ddc3a97383425bcb2eeae29d0377
Abhishek Tripathi <aktripathi@seceon.com>
2018-04-16 21:15:43 -0400

    Adding startup script for spark on k8s.

--- at_start_spark_on_k8s.sh
+++ at_start_spark_on_k8s.sh
@@ -2,15 +2,19 @@
 
 DAEMON_NAME=spark-on-k8s
 SPARK_HOME=~/platforms/spark-2.3.0-bin-hadoop2.7
-JAR=target/abhi-shade*.jar 
+
+# This startup script assumed that you have k8s cluster up and rrunning.
+# Kubeadm or minikube can be used to bring up k8s cluster easily.
+# k8s master node: k8s-master
+# Same node was also hosting docker private repo. might need to replace hostname
+# with IP address in container image.
+#   - jar path on conatiner /opt/spark/examples/jars/abhi-shade-spark-example-x.y.z.jar
+# For more details: https://spark.apache.org/docs/latest/running-on-kubernetes.html
 
 
-start() {
-        $SPARK_HOME/bin/spark-submit \
-        --name $DAEMON_NAME \
-        --master local[2] \
-        --class com.aktripathi.k8s.TryK8S \
-        $JAR
-}
-
-start
+SPARK_HOME/bin/spark-submit --master k8s://https://k8s-master:6443 \
+                            --deploy-mode cluster \
+                            --name spark-abhi \
+                            --class com.aktripathi.k8s.TryK8S \
+                            --conf spark.executor.instances=2 \
+                            --conf spark.kubernetes.container.image=k8s-master:5000/abhit/spark:2.0.5 \ local:///opt/spark/examples/jars/abhi-shade-spark-example-x.y.z.jar
